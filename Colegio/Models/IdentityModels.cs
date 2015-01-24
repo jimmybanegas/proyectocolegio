2428f64c870f03023a362dba12920b54b783d703
jimmybanegas93 <jimmy.ramos@unitec.edu>
2015-01-24 14:41:56 -0600

    Initial/ Roles, users

---
+++ Colegio/Models/IdentityModels.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity>
+            GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager
+                .CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        // Concatenate the address info for display in tables and such:
+        public string DisplayAddress
+        {
+            get
+            {
+                string dspName = string.IsNullOrWhiteSpace(this.Name) ? "" : this.Name;
+                string dspLastName = string.IsNullOrWhiteSpace(this.LastName) ? "" : this.LastName;
+                string dspUsername = string.IsNullOrWhiteSpace(this.UserName) ? "" : this.UserName;
+                return string.Format("{0} {1} {2}", dspName, dspLastName, dspUsername);
+            }
+        }
+    }
+
+
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string name) : base(name) { }
+        public string Description { get; set; }
+
+    }
+
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
